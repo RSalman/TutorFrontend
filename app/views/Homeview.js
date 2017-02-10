bc4ee59bc93e5a49b4856fbd960c2d0506e22860
Leila Compaore <wendsomleila@gmail.com>
2017-02-09 21:37:04 -0500

    sidemenu toggle

--- app/views/Homeview.js
+++ app/views/Homeview.js
@@ -1,56 +1,69 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react';
 import {
     View,
+    StyleSheet,
     Text
-} from 'react-native'
+} from 'react-native';
 import {
     Grid,
     Row,
     Button
-} from 'react-native-elements'
-class Homeview extends Component{
-    constructor(){
-        super()
-        this.state = {
-            sideIsHidden: true
-        }
-        this.pressFunct = this.pressFunct.bind(this)
+} from 'react-native-elements';
 
-    }
-    pressFunct () {
-        var newSideIsHidden = !this.state.sideIsHidden;
-        this.setState({
-            sideIsHidden: newSideIsHidden
-        });
-        this.props.onPressFunct(this.state.sideIsHidden)
+class Homeview extends Component {
+  constructor() {
+    super();
+    this.state = {
+      sideIsHidden: true
+    };
+    this.pressFunct = this.pressFunct.bind(this);
+  }
 
-    }
-    render (){
-        return (
-            <Grid>
-                <Row>
-                    <View style={{
-                        backgroundColor: 'yellow',
-                        flex: 1,
-                        flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                    }}>
-                        <Button raised
-                                icon={{name: 'cached'}}
-                                title='button icon'
-                                onPress={this.pressFunct}
-                        />
-                    </View>
-                </Row>
-                <Row>
-                    <View style={{backgroundColor: 'blue'}}>
-                        <Text>Blue background</Text>
-                    </View>
-                </Row>
-            </Grid>
+  pressFunct() {
+    var newSideIsHidden = !this.state.sideIsHidden;
+    this.setState({ sideIsHidden: newSideIsHidden });
+    this.props.onPressFunct(this.state.sideIsHidden);
 
-        )
-    }
+  }
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <View
+            style={style.container}
+          >
+            <Button
+              raised
+              icon={{ name: 'cached' }}
+              title="button icon"
+              onPress={this.pressFunct}
+            />
+          </View>
+        </Row>
+        <Row>
+          <View style={style2.container}>
+            <Text>Blue background</Text>
+          </View>
+        </Row>
+      </Grid>
+    );
+  }
 }
-export default Homeview
+
+const yellow = '#ff7733';
+const blue = '#8080ff';
+const style = StyleSheet.create({
+  container: {
+    backgroundColor: yellow,
+    flex: 1, flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+const style2 = StyleSheet.create({
+  container: {
+    backgroundColor: blue
+  }
+});
+
+export default Homeview;
--- app/views/LeilaFeatureView.js
+++ app/views/LeilaFeatureView.js
@@ -1,17 +1,56 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
 import {
-    Button
+    Button,
+    SideMenu,
+    List,
+    ListItem
 } from 'react-native-elements';
+import Homeview from './Homeview';
+
 
 export default class LeilaFeatureView extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isOpen: false
+    };
+    this.toggleSideMenu = this.toggleSideMenu.bind(this);
+  }
+
+  toggleSideMenu(isHidden) {
+    this.setState({
+      isOpen: isHidden
+    });
+  }
+
   render() {
-    return (
-      <Button
-        raised
-        icon={{ name: 'cached' }}
-        title="RAISED WITH ICON"
-      />
+    const side = (
+      <View>
+        <List>
+          <ListItem
+            title="Profile"
+            rightIcon={{ name: 'person-pin' }}
+          />
+          <ListItem
+            title="score"
+          />
+          <ListItem
+            title="Appointments"
+          />
+          <ListItem
+            title="Sign out"
+          />
+        </List>
+      </View>
     );
+    return (
+      <SideMenu
+        isOpen={this.state.isOpen}
+        menu={side}
+      >
+        <Homeview onPressFunct={this.toggleSideMenu} />
+
+      </SideMenu>);
   }
 }
