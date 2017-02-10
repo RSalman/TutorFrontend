e60e22f20cc19e8de51abff7d954813977f51cfe
Leila Compaore <wendsomleila@gmail.com>
2017-02-09 23:38:18 -0500

    structured sidemenu

--- app/views/LeilaFeatureView.js
+++ app/views/LeilaFeatureView.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import {
+    View,
+    StyleSheet,
+    Image,
+    Text
+} from 'react-native';
 import {
     SideMenu,
     List,
@@ -25,22 +30,40 @@ export default class LeilaFeatureView extends Component {
 
   render() {
     const side = (
-      <View>
-        <List>
-          <ListItem
-            title="Profile"
-            rightIcon={{ name: 'person-pin' }}
+      <View style={style.main}>
+        <View style={style.topView}>
+          <Image
+            style={style.image}
+            source={{ uri: 'https://facebook.github.io/react/img/logo_og.png' }}
           />
-          <ListItem
-            title="score"
-          />
-          <ListItem
-            title="Appointments"
-          />
-          <ListItem
-            title="Sign out"
-          />
-        </List>
+          <Text>Jane Doe</Text>
+        </View>
+        <View style={style.middleView}>
+          <List>
+            <ListItem
+              title="Profile"
+              rightIcon={{ name: 'person-pin' }}
+            />
+            <ListItem
+              title="Appointments"
+              rightIcon={{ name: 'watch-later' }}
+            />
+            <ListItem
+              title="Payments"
+              rightIcon={{ name: 'payment' }}
+            />
+            <ListItem
+              title="Help"
+              rightIcon={{ name: 'live-help' }}
+            />
+            <ListItem
+              title="Sign out"
+            />
+          </List>
+        </View>
+        <View style={style.bottomView}>
+          <Text>Terms and conditions of use</Text>
+        </View>
       </View>
     );
     return (
@@ -53,3 +76,25 @@ export default class LeilaFeatureView extends Component {
       </SideMenu>);
   }
 }
+
+const style = StyleSheet.create({
+  main: {
+    flex: 1
+  },
+  topView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  middleView: {
+    flex: 3
+  },
+  bottomView: {
+    flex: 1
+  },
+  image: {
+    width: 50, height: 50, borderRadius: 20
+  }
+});
+
