86758246ef204ff85ee7559dc6aa1e636a0ee714
Muraad Hared <mhare095@uottawa.ca>
2017-02-10 13:11:14 -0500

    Fixed Formatting

--- app/views/MuraadFeatureView.js
+++ app/views/MuraadFeatureView.js
@@ -1,17 +1,17 @@
 import ProgressBar from 'react-native-progress/Bar';
-import React from 'react';
-import t from 'tcomb-form-native';
-import { AppRegistry, StyleSheet, Text, View, TouchableHighlight, Platform, ScrollView } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableHighlight, Platform, ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import t from 'tcomb-form-native'
 
 var Form = t.form.Form;
 
 var User = t.struct({
-  first_name: t.String,              
-  last_name: t.String,  
-  email: t.String,               
-  password: t.String,               
-  phone_number: t.Number  
+  first_name: t.String,
+  last_name: t.String,
+  email: t.String,
+  password: t.String,
+  phone_number: t.Number
 });
 
 var options = {
@@ -20,7 +20,7 @@ var options = {
       error: 'Please insert a valid email'
     },
     password: {
-    	secureTextEntry: true
+      secureTextEntry: true
     },
     phone_number: {
       error: 'Please enter a valid phone number'
@@ -28,81 +28,76 @@ var options = {
   }
 };
 
-var MuraadFeatureView = React.createClass({
-	getInitialState() {
-    return {
+export default class MuraadFeatureView extends Component {
+  constructor(props) {
+    super(props);
+    /* define initial state */
+    this.state = {
       value: {
-        first_name: "",              
-			  last_name: "",  
-			  email: "",               
-			  password: "",               
-			  phone_number: ""  
+        first_name: '',
+        last_name: '',
+        email: '',
+        password: '',
+        phone_number: ''
       }
     };
-  },
-	 onChange(value) {
-    this.setState({value});
-  },
+  }
 
-  onPress: function () {
+  onChange(value) {
+    this.setState({ value });
+  }
+
+  onPress() {
     // call getValue() to get the values of the form
-    var value = this.refs.form.getValue();
-    if (value) { // if validation fails, value will be null
-       /*fetch("http://10.0.3.2:3000/auth/sign_in", {method: "POST", body: JSON.stringify(value)})
-         .then((response) => response.json())
-         .then((responseData) => {
-             alert( JSON.stringify(responseData));
-             return (<Scene key="home" component={HomeView} title="HomeView" />);
-         })
-         .done();*/
-         alert( JSON.stringify(value));
-         Actions.home();
-    }
-  },
+    var value = this.signup_form.getValue();
+    if (value)  // if validation fails, value will be null
+      /* this is where to handle form data -MH */
+      Actions.home();
 
-  render: function() {
+  }
+
+  render() {
     return (
-    	<ScrollView>
-    	<Text style={styles.header}>Sign up for Prospr</Text>
-    	<ProgressBar progress={0.3} width={320} style = {styles.progressStyling}/>
-      <View style={styles.container}>
-        <Form
-          ref="form"
-          type={User}
-          options={options}
-          onChange={this.onChange}
-          value = {this.state.value}
-        />
-        <TouchableHighlight style={styles.button} onPress={this.onPress} underlayColor='#99d9f4'>
-          <Text style={styles.buttonText}>Next</Text>
-        </TouchableHighlight>
-      </View>
-      </ScrollView> 
+      <ScrollView>
+        <Text style={styles.header}>Sign up for Prospr</Text>
+        <ProgressBar progress={0.3} width={320} style={styles.progressStyling} />
+        <View style={styles.container}>
+          <Form
+            ref={(form) => { this.signup_form = form; }}
+            type={User}
+            options={options}
+            onChange={this.onChange.bind(this)}
+            value={this.state.value}
+          />
+          <TouchableHighlight style={styles.button} onPress={this.onPress.bind(this)} underlayColor="#99d9f4">
+            <Text style={styles.buttonText}>Next</Text>
+          </TouchableHighlight>
+        </View>
+      </ScrollView>
     );
   }
-});
+}
 
-var styles = StyleSheet.create({
+/* Define colours */
+const light_blue = '#48BBEC';
+const white = '#ffffff';
+
+const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
     marginTop: 0,
     padding: 10,
-    backgroundColor: '#ffffff',
-  },
-  title: {
-    fontSize: 30,
-    alignSelf: 'center',
-    marginBottom: 30
+    backgroundColor: white,
   },
   buttonText: {
     fontSize: 18,
-    color: 'white',
+    color: white,
     alignSelf: 'center'
   },
   button: {
     height: 36,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
+    backgroundColor: light_blue,
+    borderColor: light_blue,
     borderWidth: 1,
     borderRadius: 8,
     marginBottom: 10,
@@ -110,40 +105,21 @@ var styles = StyleSheet.create({
     justifyContent: 'center'
   },
   header: {
-  	textAlign: 'center',
-  	padding: 10,
-  	fontSize: 30,
-  	fontWeight: 'bold',
-  	...Platform.select({
-       ios: {
-         fontFamily: 'HelveticaNeue-Thin'
-       },
-       android: {
-         fontFamily: 'sans-serif-thin'
-       },
-     })
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'HelveticaNeue-Thin'
+      },
+      android: {
+        fontFamily: 'sans-serif-thin'
+      },
+    })
   },
   progressStyling: {
-  	alignSelf: 'center',
-  	marginTop: 10
+    alignSelf: 'center',
+    marginTop: 10
   }
 });
-
-export default MuraadFeatureView;
-/*
-logo: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignSelf: 'center',
-    color: logoColor,
-    fontSize: 60,
-    marginTop: 10,
-    ...Platform.select({
-       ios: {
-         fontFamily: 'HelveticaNeue-Thin'
-       },
-       android: {
-         fontFamily: 'sans-serif-thin'
-       },
-     })
-  }*/
