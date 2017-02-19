ee2fb19ea865c4fb767cee1d5193e1fdcd88f799
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-02-18 22:45:10 -0500

    Add json-server to mock apis easily and moxios for tests

--- app/actions/tutors.js
+++ app/actions/tutors.js
@@ -7,13 +7,18 @@ export function getTutorsStart() {
   return { type: GET_TUTORS_START };
 }
 
-export function getTutorsComplete() {
-  return { type: GET_TUTORS_COMPLETE };
+export function getTutorsComplete(tutors) {
+  return { type: GET_TUTORS_COMPLETE, tutors };
 }
 
 export function updateTutors() {
   return dispatch => {
     dispatch(getTutorsStart());
-    dispatch(getTutorsComplete());
+    axios.get('/get_tutors').then(function(response) {
+      // TODO(sarmad): handle different codes
+      dispatch(getTutorsComplete(response.data));
+    }).catch(function(error) {
+      // TODO(sarmad): handle errors
+    });
   };
 }
--- app/reducers/tutors.js
+++ app/reducers/tutors.js
@@ -10,64 +10,7 @@ function tutors(state = initialState, action) {
     case GET_TUTORS_START:
       return { allTutors: [], isLoading: true };
     case GET_TUTORS_COMPLETE:
-      return {
-        allTutors: [
-          {
-            rate: 15,
-            picture: '',
-            rating: 5,
-            degree: 'Masters',
-          },
-          {
-            rate: 20,
-            picture: '',
-            rating: 5,
-            degree: 'Bachelors',
-          },
-          {
-            rate: 11.75,
-            picture: '',
-            rating: 4.5,
-            degree: 'PhD',
-          },
-          {
-            rate: 32.50,
-            picture: '',
-            rating: 3.9,
-            degree: 'PhD',
-          },
-          {
-            rate: 12.70,
-            picture: '',
-            rating: 1.9,
-            degree: 'Bachelors',
-          },
-          {
-            rate: 22.25,
-            picture: '',
-            rating: 2.9,
-            degree: 'Diploma',
-          },
-          {
-            rate: 32.50,
-            picture: '',
-            rating: 3.9,
-            degree: 'PhD',
-          },
-          {
-            rate: 9.50,
-            picture: '',
-            rating: 1.2,
-            degree: 'PhD',
-          },
-          {
-            rate: 25.99,
-            picture: '',
-            rating: 4.9,
-            degree: 'PhD',
-          }
-        ], isLoading: false
-      };
+      return { allTutors: action.tutors, isLoading: false };
     default:
       return state;
   }
