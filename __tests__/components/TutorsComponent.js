4f02dd871eb56b89ac64bf3001f9b800124cdb61
RSalman <srana026@uottawa.ca>
2017-02-23 22:52:56 -0500

    _test_ TutorsComponents merg fix

--- __tests__/components/TutorsComponent.js
+++ __tests__/components/TutorsComponent.js
@@ -9,38 +9,38 @@ import '../../app/util/I18n';
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 const dummyTutors = [
-    {
-        picture: '',
-        rating: 5,
-        degree: 'TEST1',
-    },
-    {
-        picture: '',
-        rating: 2,
-        degree: 'TEST2',
-    }
+  {
+    picture: '',
+    rating: 5,
+    degree: 'TEST1',
+  },
+  {
+    picture: '',
+    rating: 2,
+    degree: 'TEST2',
+  }
 ];
 
 function setup(state) {
-    const store = mockStore(state);
-    const props = {
-        updateTutors: jest.fn(),
-        store
-    };
-    return props;
+  const store = mockStore(state);
+  const props = {
+    updateTutors: jest.fn(),
+    store
+  };
+  return props;
 }
 
 describe('<TutorsComponent />', () => {
-    it('should render tutors in rows', () => {
-        const props = setup({
-            tutors: {
-                allTutors: dummyTutors,
-                isLoading: false
-            }
-        });
-        const tree = renderer.create(
-            <TutorsComponent {...props} />
-        ).toJSON();
-        expect(tree).toMatchSnapshot();
+  it('should render tutors in rows', () => {
+    const props = setup({
+      tutors: {
+        allTutors: dummyTutors,
+        isLoading: false
+      }
     });
+    const tree = renderer.create(
+      <TutorsComponent {...props} />
+        ).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
