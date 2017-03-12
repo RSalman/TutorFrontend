195c39c18a3ec148e85a60d1fc34770d200d1d96
Sarmad Hashmi <sarmadhashmi@gmail.com>
2017-03-12 04:35:14 -0400

    Display alert on no internet connection when axios requests are sent (#38)
    * Display alert on no internet connection when axios request are sent
    
    * Change to check internet connection instead of request failure, I18nify and add permission to AndroidManifest

--- app/util/globalAxios.js
+++ app/util/globalAxios.js
@@ -1,9 +1,34 @@
 import axios from 'axios';
+import { Alert, Platform, NetInfo } from 'react-native';
 
-axios.defaults.baseURL = 'http://localhost:3000/api/v1';
+axios.defaults.baseURL = `http://${Platform.OS === 'ios' ? 'localhost' : '10.0.3.2'}:3000/api/v1`;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.get['Content-Type'] = 'application/json';
 axios.defaults.headers.post.Accept = 'application/json';
 axios.defaults.headers.get.Accept = 'application/json';
+
+var connection = true;
+function connectivityChange(isConnected) {
+  connection = isConnected;
+}
+
+function showNoInternetDialog(config) {
+  return new Promise(function(resolve, reject) {
+    Alert.alert(
+      I18n.t('globalWifiDialog.noInternetConnection'),
+      I18n.t('globalWifiDialog.fixInternet'),
+      [
+        { text: I18n.t('retry'), onPress: () => resolve(axios(config)) },
+      ],
+      { cancelable: false }
+    );
+  });
+}
+
+NetInfo.isConnected.addEventListener('change', connectivityChange);
+
+axios.interceptors.response.use(undefined, function(error) {
+  return connection ? Promise.reject(error) : showNoInternetDialog(error.config);
+});
 
 global.axios = axios;
